81351378993453e7859a6212686843b33d7ef9e9
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2021-06-17 12:08:18 +0900

    Initial commit

---
+++ src/main/java/kgu/limbae/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package kgu.limbae.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
